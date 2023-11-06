8705c8fa4b80dd3751bdfcfa07e2b21954d94921
RobertAhlin <>
2023-11-06 22:47:13 +0000

    Created EventDetail(View)

--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 
@@ -8,3 +8,25 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        sign = get_object_or_404(queryset, slug=slug)
+        signed_up = sign.signed_up.filter(
+            approved=True).order_by("-created_on")
+        liked = False
+        if sign.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "sign": sign,
+                "signed_up": signed_up,
+                "liked": liked,
+            },
+        )
