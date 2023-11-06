71d3e0ba0bafab8378c2ee8bc225460dc7f42b69
RobertAhlin <>
2023-11-06 19:40:11 +0000

    Changed the allowed host for heroku

--- emxevents/settings.py
+++ emxevents/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-robertahlin-emx-lymh6zhmfc.us2.codeanyapp.com', 'robertahlin-emx-lymh6zhmfc.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-robertahlin-emx-lymh6zhmfc.us2.codeanyapp.com',
+                 'emx-6d9da8d3da50.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -85,7 +86,6 @@ WSGI_APPLICATION = 'emxevents.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 
 # Password validation
