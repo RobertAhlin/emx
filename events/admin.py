ec35ef448bd69b8ae9ae3db1dc59385c64ff63fb
RobertAhlin <>
2023-11-06 21:38:06 +0000

    Added fields and filte to EventAdmin view

--- events/admin.py
+++ events/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'event_date', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_field = ('content')
