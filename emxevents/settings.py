07cc6e8c7423c023bb42fc893cb86a5f24cbb95d
RobertAhlin <>
2023-11-07 14:17:57 +0000

    Prepare for deploy test

--- emxevents/settings.py
+++ emxevents/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-robertahlin-emx-lymh6zhmfc.us2.codeanyapp.com',
                  'emx-6d9da8d3da50.herokuapp.com', 'localhost']
