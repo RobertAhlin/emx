b6d5b7b5acd5bc4f32ba8a40f563e75a252fcf54
RobertAhlin <>
2023-11-07 12:49:31 +0000

    Changed some values to display signed up correctly

--- events/views.py
+++ events/views.py
@@ -27,7 +27,40 @@ class EventDetail(View):
             {
                 "event": event,
                 "signed_up": signed_up,
+                "signed": False,
                 "liked": liked,
                 "sign_up_form" : SignUpForm(),
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        signed_up = event.signed_up.filter(
+            approved=True).order_by("-created_on")
+        liked = False
+        if event.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        sign_up_form = SignUpForm(data=request.POST)
+
+        if sign_up_form.is_valid():
+            sign_up_form.instance.email = request.user.email
+            sign_up_form.instance.name = request.user.name
+            sign_up = sign_up_form.save(commit=False)
+            sign_up.event = event
+            sign_up.save()
+        else:
+            sign_up_form = SignUpForm()
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "event": event,
+                "signed_up": signed_up,
+                "signed": True,
+                "liked": liked,
+                "sign_up_form": SignUpForm(),
             },
         )
